d75a06b99ad646a3148ea08ed20e84d1a452756b
emmett1 <emmett1.2miligrams@gmail.com>
2018-10-22 11:32:13 +0800

    updated

--- virootfs/root/custom_script.sh
+++ virootfs/root/custom_script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 USER=venom
+PASSWORD=venom
 
-chmod -R 775 /etc/skel/.config
-mkdir /home/venom
+useradd -m -G users,wheel,audio,video -s /bin/bash $USER
+passwd -d $USER &>/dev/null
+passwd -d root &>/dev/null
 
-sed -i 's/localhost/venomlive/' /etc/rc.conf
+echo "root:root" | chpasswd -c SHA512
+echo "$USER:$PASSWORD" | chpasswd -c SHA512
+
+chmod -R 775 /home/$USER/.config
 
 # autologin user
 sed -i "s/#autologin-user=/autologin-user=$USER/" /etc/lightdm/lightdm.conf
@@ -16,9 +21,22 @@ sed -i "s:#background=:background=/usr/share/backgrounds/venom1.jpg:" /etc/light
 sed -i "s:#theme-name=:theme-name=Arc-Darker:" /etc/lightdm/lightdm-gtk-greeter.conf
 sed -i "s:#icon-theme-name=:icon-theme-name=Papirus-Dark:" /etc/lightdm/lightdm-gtk-greeter.conf
 
+sed -i 's/localhost/venomlive/' /etc/rc.conf
 sed -i '/DAEMONS=(/,/)/d' /etc/rc.conf
 
-for i in sysklogd dbus lightdm networkmanager bluetooth; do
+if [ -x /etc/rc.d/lxdm ]; then
+	DM=lxdm
+elif [ -x /etc/rc.d/lightdm ]; then
+	DM=lightdm
+fi
+
+if [ -x /etc/rc.d/networkmanager ]; then
+	NETWORK=networkmanager
+elif [ -x /etc/rc.d/network ]; then
+	NETWORK=network
+fi
+
+for i in sysklogd dbus $DM $NETWORK bluetooth; do
 	if [ -x /etc/rc.d/$i ]; then
 		daemon+=($i)
 	fi
