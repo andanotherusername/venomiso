20006bd4776b70c3bd1b8c23f3c8ab2220db4f51
emmett1 <emmett1.2miligrams@gmail.com>
2019-06-15 00:02:14 +0800

    fix scriptss

--- buildall.sh
+++ buildall.sh
@@ -10,14 +10,14 @@
 # currently support edition is base, xorg, mate, lxde, and xfce4
 #
 
-MUST_PKG="linux wpa_supplicant os-prober linux-firmware"          # must have pkg in the iso
-XORG_PKG="xorg xorg-video-drivers xf86-input-libinput"      # xorg stuff in the iso
-EXTRA_PKG="$(cat pkglist | grep -v ^#)"                     # extra stuff in the iso, read from 'pkglist' file in current dir
+MUST_PKG="linux wpa_supplicant os-prober linux-firmware grub"    # must have pkg in the iso
+XORG_PKG="xorg xorg-video-drivers xf86-input-libinput"           # xorg stuff in the iso
+EXTRA_PKG="$(cat pkglist | grep -v ^#)"                          # extra stuff in the iso, read from 'pkglist' file in current dir
 
-PKG_DIR="/mnt/data/venom/packages-dev/"                     # prebuilt pkg path in host
-SRC_DIR="/mnt/data/venom/sources/"                          # source path in host
+PKG_DIR="/mnt/data/venom/packages-dev/"                          # prebuilt pkg path in host
+SRC_DIR="/mnt/data/venom/sources/"                               # source path in host
 
-BUILD_CMD="sudo ./mkiso -r -p $PKG_DIR -s $SRC_DIR"         # cmd used by 'mkiso' script
+BUILD_CMD="sudo ./mkiso -r -p $PKG_DIR -s $SRC_DIR"              # cmd used by 'mkiso' script
 
 if [ "$*" ]; then
 	while [ "$1" ]; do
@@ -29,7 +29,7 @@ if [ "$*" ]; then
 		shift
 	done
 else
-	EDITION="base xorg lxde mate xfce4"
+	EDITION="rootfs base xorg lxde mate xfce4"
 fi
 
 for i in $EDITION; do
@@ -37,12 +37,13 @@ for i in $EDITION; do
 		echo ":: Skipping venom-$i-$(date +"%Y%m%d").iso"
 	else
 		case $i in
-			base)  BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			xorg)  BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			mate)  BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG mate mate-extra lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			lxde)  BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG lxde lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			xfce4) BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG xfce4 lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			*)     echo "ERROR: Currently suport flavors is base, xorg, mate, lxde, and xfce4"; exit 1 ;;
+			rootfs) BUILD_CMD_FINAL="sudo ./mkrootfs -p $PKG_DIR -s $SRC_DIR -o venom-rootfs.txz" ;;
+			base)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
+			xorg)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
+			mate)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG mate mate-extra lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
+			lxde)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG lxde lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
+			xfce4)  BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG xfce4 lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
+			*)      echo "ERROR: Currently suport flavors is rootfs, base, xorg, mate, lxde, and xfce4"; exit 1 ;;
 		esac
 		echo ":: Building '$i' flavor..."
 		$BUILD_CMD_FINAL || error=1
@@ -52,3 +53,5 @@ for i in $EDITION; do
 		exit 1
 	fi
 done
+
+exit 0
