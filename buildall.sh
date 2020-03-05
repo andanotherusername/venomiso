ca9bb12757e647ebce45b115c52a35d5c7911855
emmett1 <emmett1.2miligrams@gmail.com>
2020-03-05 17:26:44 +0800

    updated

--- buildall.sh
+++ buildall.sh
@@ -12,13 +12,17 @@
 
 MUST_PKG="linux wpa_supplicant os-prober grub"                   # must have pkg in the iso
 XORG_PKG="xorg xorg-video-drivers xf86-input-libinput"           # xorg stuff in the iso
-EXTRA_PKG="$(cat pkglist | grep -v ^#)"                          # extra stuff in the iso, read from 'pkglist' file in current dir
+EXTRA_PKG="$(grep -v ^# pkglist | tr '\n' ' ')"                  # extra stuff in the iso, read from 'pkglist' file in current dir
 
 PKG_DIR="/mnt/data/venom/packages-dev/"                          # prebuilt pkg path in host
 SRC_DIR="/mnt/data/venom/sources/"                               # source path in host
 
 # cmd used by 'mkiso' script
-BUILD_CMD="sudo WDIR=$WDIR VENOMSRC=$VENOMSRC ./mkiso -r -p $PKG_DIR -s $SRC_DIR"
+BUILD_CMD="sudo ./build -pkgdir $PKG_DIR -srcdir $SRC_DIR"
+
+[ -f output/venom-rootfs.txz ] && {
+	VENOMSRCOPT="output/venom-rootfs.txz"
+}
 
 if [ "$*" ]; then
 	while [ "$1" ]; do
@@ -34,25 +38,25 @@ else
 fi
 
 for i in $EDITION; do
-	if [ -f venom-$i-$(date +"%Y%m%d").iso ]; then
+	if [ -f output/venom-$i-$(date +"%Y%m%d").iso ]; then
 		echo ":: Skipping venom-$i-$(date +"%Y%m%d").iso"
 	else
+		OUTPUT=venom-$i-$(date +"%Y%m%d")
+		ISOOPT="-iso"
 		case $i in
-			rootfs) BUILD_CMD_FINAL="sudo ./mkrootfs -p $PKG_DIR -s $SRC_DIR -o venom-rootfs.txz" ;;
-			base)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			xorg)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			mate)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG mate mate-extra lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			lxde)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG lxde lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			lxqt)   BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG openbox lxqt oxygen-icons5 lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
-			xfce4)  BUILD_CMD_FINAL="$BUILD_CMD -P $MUST_PKG $XORG_PKG xfce4 lxdm $EXTRA_PKG -o venom-$i-$(date +"%Y%m%d").iso";;
+			base)   PKGS="-pkg $MUST_PKG";;
+			xorg)   PKGS="-pkg $MUST_PKG $XORG_PKG";;
+			mate)   PKGS="-pkg $MUST_PKG $XORG_PKG $EXTRA_PKG mate mate-extra lxdm";;
+			lxde)   PKGS="-pkg $MUST_PKG $XORG_PKG $EXTRA_PKG lxde lxdm";;
+			lxqt)   PKGS="-pkg $MUST_PKG $XORG_PKG $EXTRA_PKG openbox lxqt oxygen-icons5 lxdm";;
+			xfce4)  PKGS="-pkg $MUST_PKG $XORG_PKG $EXTRA_PKG xfce4 lxdm";;
+			rootfs) PKGS="" ISOOPT="" OUTPUT="venom-rootfs";;
 			*)      echo "ERROR: Currently suport flavors is rootfs, base, xorg, mate, lxde, and xfce4"; exit 1 ;;
 		esac
+		echo "build command: $BUILD_CMD $PKGS $ISOOPT -output $OUTPUT $VENOMSRCOPT"
+		sleep 3
 		echo ":: Building '$i' flavor..."
-		$BUILD_CMD_FINAL || error=1
-		unset BUILD_CMD_FINAL
-	fi
-	if [ "$error" = 1 ]; then
-		exit 1
+		$BUILD_CMD $PKGS $ISOOPT -output $OUTPUT $VENOMSRCOPT || exit 1
 	fi
 done
 
